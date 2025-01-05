2a381b7151024b2dea99932e8df224992de45cb4
kyawzinkoko <kyawzinkoko.dev@gmail.com>
2025-01-06 00:35:43 +0630

    implement product image

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -10,6 +10,8 @@ use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -23,7 +25,7 @@ use Spatie\Permission\Models\Role;
 class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
-
+    protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
     protected static ?string $navigationIcon = 'heroicon-o-cube';
 
     public static function form(Form $form): Form
@@ -37,52 +39,52 @@ class ProductResource extends Resource
                         $set('slug', Str::slug($state));
                     }),
                 TextInput::make('slug')
-                ->required(),
+                    ->required(),
                 Select::make('department_id')
-                ->relationship('department', 'name')
-                ->label(__('Department'))
-                ->preload()
-                ->reactive() //make the field reactive to change
-                ->required()
-                ->afterStateUpdated(function (string $operation, $state, callable $set) {
-                    $set('category_id',null); //reset the category when the product change
-                }),
+                    ->relationship('department', 'name')
+                    ->label(__('Department'))
+                    ->preload()
+                    ->reactive() //make the field reactive to change
+                    ->required()
+                    ->afterStateUpdated(function (string $operation, $state, callable $set) {
+                        $set('category_id', null); //reset the category when the product change
+                    }),
                 Select::make('category_id')
-                ->relationship(name:'category',titleAttribute: 'name',modifyQueryUsing: function(Builder $query,callable $get)  {
-                    $departmentId= $get('department_id');
-                    if($departmentId){
-                        $query->where('department_id',$departmentId);
-                    }
-                })
-                ->label(__('Category'))
-                ->preload()
-                ->searchable()
-                ->required(),
+                    ->relationship(name: 'category', titleAttribute: 'name', modifyQueryUsing: function (Builder $query, callable $get) {
+                        $departmentId = $get('department_id');
+                        if ($departmentId) {
+                            $query->where('department_id', $departmentId);
+                        }
+                    })
+                    ->label(__('Category'))
+                    ->preload()
+                    ->searchable()
+                    ->required(),
                 Forms\Components\RichEditor::make('description')
-                ->required()
-                ->toolbarButtons([
-                    'blockquote',
-                    'bold',
-                    'bulletList',
-                    'h2',
-                    'h3',
-                    'italic',
-                    'link',
-                    'orderList',
-                    'redo',
-                    'strike',
-                    'underline',
-                    'undo',
-                    'table'
-                ])
-                ->columnSpan(2),
+                    ->required()
+                    ->toolbarButtons([
+                        'blockquote',
+                        'bold',
+                        'bulletList',
+                        'h2',
+                        'h3',
+                        'italic',
+                        'link',
+                        'orderList',
+                        'redo',
+                        'strike',
+                        'underline',
+                        'undo',
+                        'table'
+                    ])
+                    ->columnSpan(2),
                 Forms\Components\TextInput::make('price')
-                ->numeric()
-                ->required(),
+                    ->numeric()
+                    ->required(),
                 Forms\Components\TextInput::make('quantity')
-                ->integer(),
+                    ->integer(),
                 Forms\Components\Select::make('status')
-                ->options(ProductStatusEnum::labels()),
+                    ->options(ProductStatusEnum::labels()),
             ]);
     }
 
@@ -90,22 +92,26 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                    TextColumn::make('name')
-                ->words(10),
-                    TextColumn::make('status')
-                ->badge()
-                ->colors(ProductStatusEnum::colors()),
+                TextColumn::make('name')
+                    ->words(10),
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('images')
+                    ->collection('images')
+                    ->label(__('Image'))
+                    ->conversion('thumb'),
+                TextColumn::make('status')
+                    ->badge()
+                    ->colors(ProductStatusEnum::colors()),
                 TextColumn::make('department.name'),
                 TextColumn::make('category.name'),
                 TextColumn::make('created_at')
-                ->dateTime(),
+                    ->dateTime(),
             ])
             ->searchable()
             ->filters([
                 SelectFilter::make('status')
-                ->options(ProductStatusEnum::labels()),
+                    ->options(ProductStatusEnum::labels()),
                 SelectFilter::make('Department Id')
-                ->relationship('department', 'name')
+                    ->relationship('department', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -124,12 +130,21 @@ class ProductResource extends Resource
         ];
     }
 
+    public static function getRecordSubNavigation(Page $page): array
+    {
+        return $page->generateNavigationItems([
+            Pages\EditProduct::class,
+            Pages\ProductImage::class
+        ]);
+    }
+
     public static function getPages(): array
     {
         return [
             'index' => Pages\ListProducts::route('/'),
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
+            'images' => Pages\ProductImage::route('/{record}/images'),
         ];
     }
 //    public static function canViewAny(): bool
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,14 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    public function department(): BelongsTo{
+    use InteractsWithMedia;
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(100);
+        $this->addMediaConversion('small')
+            ->width(480);
+        $this->addMediaConversion('large')
+            ->width(1200);
+    }
+
+    public function department(): BelongsTo
+    {
         return $this->belongsTo(Department::class);
     }
 
-   public function category():belongsTo
+    public function category(): belongsTo
     {
         return $this->belongsTo(Category::class);
     }
